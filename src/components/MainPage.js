864f42d620d7035b94338897a223ee6decf3558a
Sunwoo Kim <sunwoo.kim.2@stonybrook.edu>
2024-03-24 01:28:24 +0900

    Main page first draft

---
+++ src/components/MainPage.js
@@ -0,0 +1,131 @@
+import React from "react";
+import "../css/MainPage.css";
+
+export const MainPage = () => {
+  return (
+    <div className="login-page">
+      <div className="group">
+        <div className="text-wrapper">Easy Course Scheduling</div>
+        <button className="button">
+          <div className="div">Google Sign in</div>
+          <img className="element-logo-google" alt="Element logo google" src="7123025-logo-google-g-icon-1.png" />
+        </button>
+        <header className="header">
+          <div className="text-wrapper-2">Course Schedule</div>
+        </header>
+        <p className="p">Sign in free with Google</p>
+        <div className="group-2">
+          <img className="element-am" alt="Element am" src="7-am.svg" />
+          <img className="img" alt="Element am" src="8-am.svg" />
+          <img className="element-am-2" alt="Element am" src="9-am.svg" />
+          <img className="element-am-3" alt="Element am" src="10-am.svg" />
+          <img className="element-am-4" alt="Element am" src="11-am.svg" />
+          <img className="element-pm" alt="Element pm" src="12-pm.svg" />
+          <img className="element-pm-2" alt="Element pm" src="1-pm.svg" />
+          <img className="element-pm-3" alt="Element pm" src="2-pm.svg" />
+          <img className="element-pm-4" alt="Element pm" src="3-pm.svg" />
+          <img className="element-pm-5" alt="Element pm" src="4-pm.svg" />
+          <img className="element-pm-6" alt="Element pm" src="5-pm.svg" />
+          <img className="element-pm-7" alt="Element pm" src="6-pm.svg" />
+          <img className="element-pm-8" alt="Element pm" src="7-pm.svg" />
+        </div>
+        <div className="group-3">
+          <img className="shape-with-text" alt="Shape with text" src="shape-with-text.svg" />
+          <img className="shape-with-text-2" alt="Shape with text" src="image.svg" />
+          <img className="shape-with-text-3" alt="Shape with text" src="shape-with-text-2.svg" />
+          <img className="shape-with-text-4" alt="Shape with text" src="shape-with-text-3.svg" />
+          <img className="shape-with-text-5" alt="Shape with text" src="shape-with-text-4.svg" />
+          <img className="shape-with-text-6" alt="Shape with text" src="shape-with-text-5.svg" />
+          <img className="shape-with-text-7" alt="Shape with text" src="shape-with-text-6.svg" />
+          <img className="shape-with-text-8" alt="Shape with text" src="shape-with-text-7.svg" />
+          <img className="shape-with-text-9" alt="Shape with text" src="shape-with-text-8.svg" />
+          <img className="shape-with-text-10" alt="Shape with text" src="shape-with-text-9.svg" />
+          <img className="shape-with-text-11" alt="Shape with text" src="shape-with-text-10.svg" />
+          <img className="shape-with-text-12" alt="Shape with text" src="shape-with-text-11.svg" />
+          <img className="shape-with-text-13" alt="Shape with text" src="shape-with-text-12.svg" />
+        </div>
+        <div className="group-4">
+          <img className="shape-with-text" alt="Shape with text" src="shape-with-text-13.svg" />
+          <img className="shape-with-text-2" alt="Shape with text" src="shape-with-text-14.svg" />
+          <img className="shape-with-text-3" alt="Shape with text" src="shape-with-text-15.svg" />
+          <img className="shape-with-text-4" alt="Shape with text" src="shape-with-text-16.svg" />
+          <img className="shape-with-text-5" alt="Shape with text" src="shape-with-text-17.svg" />
+          <img className="shape-with-text-6" alt="Shape with text" src="shape-with-text-18.svg" />
+          <img className="shape-with-text-7" alt="Shape with text" src="shape-with-text-19.svg" />
+          <img className="shape-with-text-8" alt="Shape with text" src="shape-with-text-20.svg" />
+          <img className="shape-with-text-9" alt="Shape with text" src="shape-with-text-21.svg" />
+          <img className="shape-with-text-10" alt="Shape with text" src="shape-with-text-22.svg" />
+          <img className="shape-with-text-11" alt="Shape with text" src="shape-with-text-23.svg" />
+          <img className="shape-with-text-12" alt="Shape with text" src="shape-with-text-24.svg" />
+          <img className="shape-with-text-13" alt="Shape with text" src="shape-with-text-25.svg" />
+        </div>
+        <div className="group-5">
+          <img className="shape-with-text" alt="Shape with text" src="shape-with-text-26.svg" />
+          <img className="shape-with-text-2" alt="Shape with text" src="shape-with-text-27.svg" />
+          <img className="shape-with-text-3" alt="Shape with text" src="shape-with-text-28.svg" />
+          <img className="shape-with-text-4" alt="Shape with text" src="shape-with-text-29.svg" />
+          <img className="shape-with-text-5" alt="Shape with text" src="shape-with-text-30.svg" />
+          <img className="shape-with-text-6" alt="Shape with text" src="shape-with-text-31.svg" />
+          <img className="shape-with-text-7" alt="Shape with text" src="shape-with-text-32.svg" />
+          <img className="shape-with-text-8" alt="Shape with text" src="shape-with-text-33.svg" />
+          <img className="shape-with-text-9" alt="Shape with text" src="shape-with-text-34.svg" />
+          <img className="shape-with-text-10" alt="Shape with text" src="shape-with-text-35.svg" />
+          <img className="shape-with-text-11" alt="Shape with text" src="shape-with-text-36.svg" />
+          <img className="shape-with-text-12" alt="Shape with text" src="shape-with-text-37.svg" />
+          <img className="shape-with-text-13" alt="Shape with text" src="shape-with-text-38.svg" />
+        </div>
+        <div className="group-6">
+          <img className="shape-with-text" alt="Shape with text" src="shape-with-text-39.svg" />
+          <img className="shape-with-text-2" alt="Shape with text" src="shape-with-text-40.svg" />
+          <img className="shape-with-text-3" alt="Shape with text" src="shape-with-text-41.svg" />
+          <img className="shape-with-text-4" alt="Shape with text" src="shape-with-text-42.svg" />
+          <img className="shape-with-text-5" alt="Shape with text" src="shape-with-text-43.svg" />
+          <img className="shape-with-text-6" alt="Shape with text" src="shape-with-text-44.svg" />
+          <img className="shape-with-text-7" alt="Shape with text" src="shape-with-text-45.svg" />
+          <img className="shape-with-text-8" alt="Shape with text" src="shape-with-text-46.svg" />
+          <img className="shape-with-text-9" alt="Shape with text" src="shape-with-text-47.svg" />
+          <img className="shape-with-text-10" alt="Shape with text" src="shape-with-text-48.svg" />
+          <img className="shape-with-text-11" alt="Shape with text" src="shape-with-text-49.svg" />
+          <img className="shape-with-text-12" alt="Shape with text" src="shape-with-text-50.svg" />
+          <img className="shape-with-text-13" alt="Shape with text" src="shape-with-text-51.svg" />
+        </div>
+        <div className="group-7">
+          <img className="shape-with-text" alt="Shape with text" src="shape-with-text-52.svg" />
+          <img className="shape-with-text-2" alt="Shape with text" src="shape-with-text-53.svg" />
+          <img className="shape-with-text-3" alt="Shape with text" src="shape-with-text-54.svg" />
+          <img className="shape-with-text-4" alt="Shape with text" src="shape-with-text-55.svg" />
+          <img className="shape-with-text-5" alt="Shape with text" src="shape-with-text-56.svg" />
+          <img className="shape-with-text-6" alt="Shape with text" src="shape-with-text-57.svg" />
+          <img className="shape-with-text-7" alt="Shape with text" src="shape-with-text-58.svg" />
+          <img className="shape-with-text-8" alt="Shape with text" src="shape-with-text-59.svg" />
+          <img className="shape-with-text-9" alt="Shape with text" src="shape-with-text-60.svg" />
+          <img className="shape-with-text-10" alt="Shape with text" src="shape-with-text-61.svg" />
+          <img className="shape-with-text-11" alt="Shape with text" src="shape-with-text-62.svg" />
+          <img className="shape-with-text-12" alt="Shape with text" src="shape-with-text-63.svg" />
+          <img className="shape-with-text-13" alt="Shape with text" src="shape-with-text-64.svg" />
+        </div>
+        <div className="text-wrapper-3">Monday</div>
+        <div className="text-wrapper-4">Tuesday</div>
+        <div className="text-wrapper-5">Wednesday</div>
+        <div className="text-wrapper-6">Thrusday</div>
+        <div className="text-wrapper-7">Friday</div>
+        <div className="overlap-group">
+          <img className="cse" alt="Cse" src="cse301.svg" />
+          <img className="cse-2" alt="Cse" src="cse310.svg" />
+          <img className="cse-3" alt="Cse" src="cse373.svg" />
+        </div>
+        <img className="cse-4" alt="Cse" src="cse216.svg" />
+        <img className="cse-5" alt="Cse" src="cse101.svg" />
+        <img className="cse-6" alt="Cse" src="cse215.svg" />
+        <img className="cse-7" alt="Cse" src="cse114.svg" />
+        <img className="cse-8" alt="Cse" src="cse216-2.svg" />
+        <img className="cse-9" alt="Cse" src="cse215-2.svg" />
+        <img className="cse-10" alt="Cse" src="cse114-2.svg" />
+        <img className="cse-11" alt="Cse" src="cse214.svg" />
+      </div>
+    </div>
+  );
+};
+
+
+export default MainPage;
